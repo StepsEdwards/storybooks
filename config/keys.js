3f023d913217fa9768fc464dd4b9be1e85e2ee57
Stephens Jean-Jacques <stepsedwards@gmail.com>
2018-01-19 19:54:15 -0500

    started UI

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI:'mongodb://StepsEdwards:edwards6@ds157097.mlab.com:57097/storybooks-dev',
+    mongoURI:'mongodb://StepsEdwards:edwards@ds157097.mlab.com:57097/storybooks-dev',
     googleClientID:'670747336887-043engggq91prjp1qvl1p02odprj4uqj.apps.googleusercontent.com',
     googleClientSecret:'Mcw-MONEliDDUF5jkqJPf8cQ'
 }
--- routes/auth.js
+++ routes/auth.js
@@ -5,8 +5,21 @@ const passport = require('passport');
 // routes to "auth/google" because it's in the auth.js file
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), (req, res) => {
+router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
         res.redirect('/dashboard');
     });
+
+router.get('/verify', (req, res) =>{
+    if(req.user){
+        console.log(req.user);
+    } else {
+        console.log('Not Authorized');
+    }
+});
+
+router.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+});
 
 module.exports = router;
